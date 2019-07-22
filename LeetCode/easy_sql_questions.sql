ee03a351940f33ac3639430c7b0f71e9a20d18db
Ehsan Negahbani <38743253+Ehsan-Negahbani@users.noreply.github.com>
2019-07-22 16:16:58 -0700

    Update easy_sql_questions.sql

--- LeetCode/easy_sql_questions.sql
+++ LeetCode/easy_sql_questions.sql
@@ -56,4 +56,71 @@ WHERE NOT EXISTS (SELECT * FROM Orders o WHERE o.CustomerId=c.id)
 
 
 
--- # 2. 
+-- # 2. There is a table World:
+-- +-----------------+------------+------------+--------------+---------------+
+-- | name            | continent  | area       | population   | gdp           |
+-- +-----------------+------------+------------+--------------+---------------+
+-- | Afghanistan     | Asia       | 652230     | 25500100     | 20343000      |
+-- | Albania         | Europe     | 28748      | 2831741      | 12960000      |
+-- | Algeria         | Africa     | 2381741    | 37100000     | 188681000     |
+-- | Andorra         | Europe     | 468        | 78115        | 3712000       |
+-- | Angola          | Africa     | 1246700    | 20609294     | 100990000     |
+-- +-----------------+------------+------------+--------------+---------------+
+-- A country is big if it has an area of bigger than 3 million square km or a population of more than 25 million.
+-- Write a SQL solution to output big countries' name, population and area.
+-- Solution 1:
+SELECT name, population, area
+FROM World
+WHERE (area>3000000 OR population >25000000)
+--Solution 2 using Union, which is faster at least in this exmple:
+SELECT name, population, area
+FROM World
+WHERE area > 3000000 
+
+UNION
+
+SELECT name, population, area
+FROM World
+WHERE population > 25000000
+
+
+-- # 3. Delete Duplicate Emails:
+-- Write a SQL query to delete all duplicate email entries in a table named Person, 
+-- keeping only unique emails based on its smallest Id.
+-- +----+------------------+
+-- | Id | Email            |
+-- +----+------------------+
+-- | 1  | john@example.com |
+-- | 2  | bob@example.com  |
+-- | 3  | john@example.com |
+-- +----+------------------+
+-- Id is the primary key column for this table.
+-- For example, after running your query, the above Person table should have the following rows:
+
+-- +----+------------------+
+-- | Id | Email            |
+-- +----+------------------+
+-- | 1  | john@example.com |
+-- | 2  | bob@example.com  |
+-- +----+------------------+
+
+-- Solution1:
+DELETE p1.* FROM Person p1 JOIN
+    Person p2 on p1.email=p2.email
+WHERE
+    p1.Email = p2.Email AND p1.Id > p2.Id
+    
+-- Solution2 (the poiont is to use a temp table since you can not modify the primary key column on the main table):
+DELETE FROM Person
+WHERE Id NOT IN (
+  SELECT * FROM (
+    SELECT Min(Id)
+    FROM Person
+    GROUP BY Email
+  ) Tmp
+)
+
+
+
+
+
