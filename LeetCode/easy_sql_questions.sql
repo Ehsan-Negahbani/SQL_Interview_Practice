9055deaa4cdcc5b8e869b716bc27485e0161432b
Ehsan Negahbani <38743253+Ehsan-Negahbani@users.noreply.github.com>
2019-07-29 08:25:52 -0700

    Update easy_sql_questions.sql

--- LeetCode/easy_sql_questions.sql
+++ LeetCode/easy_sql_questions.sql
@@ -120,6 +120,50 @@ WHERE Id NOT IN (
   ) Tmp
 )
 
+-- # 4. Second Highest Salary:
+-- Write a SQL query to get the second highest salary from the employee table.
+-- +----+--------+
+-- | Id | Salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- | 2  | 200    |
+-- | 3  | 300    |
+-- +----+--------+
+
+-- For example, given the above Employee table, the query should return 200 as the second highest salary. 
+-- If there is no second highest salary, then the query should return null.
+-- +---------------------+
+-- | SecondHighestSalary |
+-- +---------------------+
+-- | 200                 |
+-- +---------------------+
+
+-- Solution1: 
+SELECT Max(Salary) as SecondHighestSalary
+FROM Employee
+WHERE Salary =ANY
+(
+SELECT Salary
+FROM Employee
+WHERE Salary<
+(
+    SELECT MAX(Salary)
+    FROM Employee
+)
+)
+
+--Solution2: 
+
+
+
+
+
+
+
+
+
+
+
 
 
 
