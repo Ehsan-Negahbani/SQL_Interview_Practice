9fb64491aa8f1eda46bfc8b6dc62da8a96d865f9
Ehsan Negahbani <38743253+Ehsan-Negahbani@users.noreply.github.com>
2019-07-31 10:21:12 -0700

    Update easy_sql_questions.sql

--- LeetCode/easy_sql_questions.sql
+++ LeetCode/easy_sql_questions.sql
@@ -341,6 +341,12 @@ HAVING COUNT(1) > 1
 -- |  4 |
 -- +----+
 
+-- Solution 1
+SELECT w2.Id
+FROM Weather w1 JOIN Weather w2
+ON  DATEDIFF(w2.RecordDate, w1.RecordDate)=1
+WHERE w2.Temperature>w1.Temperature
+
 
 
 
