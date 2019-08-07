b598662b62b5497de0fbac56ee69d733e21c0a27
Ehsan Negahbani <38743253+Ehsan-Negahbani@users.noreply.github.com>
2019-08-06 17:43:07 -0700

    Create selected_pgexercises.sql

---
+++ selected_pgexercises.sql
@@ -0,0 +1,16 @@
+-- # 1. 
+-- ref: https://pgexercises.com/questions/joins/sub.html
+-- How can you output a list of all members, 
+-- including the individual who recommended them (if any), 
+-- without using any joins? Ensure that there are no duplicates in the list, 
+-- and that each firstname + surname pairing is formatted as a column and ordered.
+
+
+-- # 2. 
+-- ref: https://pgexercises.com/questions/joins/threejoin2.html
+-- How can you produce a list of bookings on the day of 2012-09-14 
+-- which will cost the member (or guest) more than $30? Remember that guests 
+-- have different costs to members (the listed costs are per half-hour 'slot'), 
+-- and the guest user is always ID 0. Include in your output the name of the facility, 
+-- the name of the member formatted as a single column, and the cost. Order by descending cost, 
+-- and do not use any subqueries.
