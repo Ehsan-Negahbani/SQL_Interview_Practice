64b4a23823318da9275d47aa73519f5ffac246a4
Ehsan Negahbani <38743253+Ehsan-Negahbani@users.noreply.github.com>
2019-08-13 12:41:20 -0700

    Update medium_sql_questions.sql

--- LeetCode/medium_sql_questions.sql
+++ LeetCode/medium_sql_questions.sql
@@ -29,3 +29,36 @@ WHERE Id=ANY(
               ON l3.Id=l2.Id+1 AND l2.Id=l1.Id+1
               WHERE l1.Num=l2.Num AND l2.Num=l3.Num
             )
+            
+            
+-- # 2. Nth Highest Salary
+-- Write a SQL query to get the nth highest salary from the Employee table
+-- +----+--------+
+-- | Id | Salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- | 2  | 200    |
+-- | 3  | 300    |
+-- +----+--------+
+-- For example, given the above Employee table, the nth highest salary where n = 2 is 200. 
+-- If there is no nth highest salary, then the query should return null.
+-- +------------------------+
+-- | getNthHighestSalary(2) |
+-- +------------------------+
+-- | 200                    |
+-- +------------------------+
+
+-- Solution 1:
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+ DECLARE M INT;
+SET M=N-1; 
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT DISTINCT Salary getNthHighestSalary
+      FROM Employee
+      ORDER BY Salary DESC
+      LIMIT 1 OFFSET M
+  );
+END
+
