7424d7e5d55fa03cc6fd5f3de466d914773bd69b
Ehsan Negahbani <38743253+Ehsan-Negahbani@users.noreply.github.com>
2019-08-16 08:43:53 -0700

    Update medium_sql_questions.sql
    Added question number #3. It was challenging, I did not know you can check on two columns simultaneously!

--- LeetCode/medium_sql_questions.sql
+++ LeetCode/medium_sql_questions.sql
@@ -62,3 +62,48 @@ SET M=N-1;
   );
 END
 
+
+-- # 3. Department Higheswt Salary
+-- The Employee table holds all employees. 
+-- Every employee has an Id, a salary, and there is also a column for the department Id.
+
+-- +----+-------+--------+--------------+
+-- | Id | Name  | Salary | DepartmentId |
++----+-------+--------+--------------+
+-- | 1  | Joe   | 70000  | 1            |
+-- | 2  | Jim   | 90000  | 1            |
+-- | 3  | Henry | 80000  | 2            |
+-- | 4  | Sam   | 60000  | 2            |
+-- | 5  | Max   | 90000  | 1            |
+-- +----+-------+--------+--------------+
+-- The Department table holds all departments of the company.
+
+-- +----+----------+
+-- | Id | Name     |
+-- +----+----------+
+-- | 1  | IT       |
+-- | 2  | Sales    |
+-- +----+----------+
+
+-- Write a SQL query to find employees who have the highest salary in each of the departments. 
+-- For the above tables, your SQL query should return the following rows (order of rows does not matter).
+
+-- +------------+----------+--------+
+-- | Department | Employee | Salary |
+-- +------------+----------+--------+
+-- | IT         | Max      | 90000  |
+-- | IT         | Jim      | 90000  |
+-- | Sales      | Henry    | 80000  |
+-- +------------+----------+--------+
+
+-- Solution 1 (my solution)
+-- Challenge was knowing how to compare two columnsof a table with another table
+-- The solution uses a JOIN (easy part) and a subquery (Easy) with comparison (Challenging)
+SELECT d.Name Department, e1.Name Employee, e1.Salary Salary
+FROM employee e1 JOIN Department d
+ON e1.DepartmentId=d.Id
+WHERE (e1.Salary, e1.DepartmentId) = ANY(
+    SELECT  MAX(e1.Salary), e1.DepartmentId
+    FROM Employee e1
+    GROUP BY e1.DepartmentId
+    )
