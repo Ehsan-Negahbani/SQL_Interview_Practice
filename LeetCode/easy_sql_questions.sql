bc12e000552389e61a1dc3c3c6b66ec67f3e751e
Ehsan Negahbani <38743253+Ehsan-Negahbani@users.noreply.github.com>
2019-09-10 17:08:19 -0700

    Update easy_sql_questions.sql

--- LeetCode/easy_sql_questions.sql
+++ LeetCode/easy_sql_questions.sql
@@ -428,6 +428,81 @@ GROUP BY actor_id, director_id
 HAVING(COUNT(timestamp)>=3)
 
 
+-- # 12. Project Employees I
+
+-- Table: Project
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | project_id  | int     |
+-- | employee_id | int     |
+-- +-------------+---------+
+-- (project_id, employee_id) is the primary key of this table.
+-- employee_id is a foreign key to Employee table.
+-- Table: Employee
+
+-- +------------------+---------+
+-- | Column Name      | Type    |
+-- +------------------+---------+
+-- | employee_id      | int     |
+-- | name             | varchar |
+-- | experience_years | int     |
+-- +------------------+---------+
+-- employee_id is the primary key of this table.
+ 
+
+-- Write an SQL query that reports the average experience years of all the employees for each project, rounded to 2 digits.
+
+-- The query result format is in the following example:
+
+-- Project table:
+-- +-------------+-------------+
+-- | project_id  | employee_id |
+-- +-------------+-------------+
+-- | 1           | 1           |
+-- | 1           | 2           |
+-- | 1           | 3           |
+-- | 2           | 1           |
+-- | 2           | 4           |
+-- +-------------+-------------+
+
+-- Employee table:
+-- +-------------+--------+------------------+
+-- | employee_id | name   | experience_years |
+-- +-------------+--------+------------------+
+-- | 1           | Khaled | 3                |
+-- | 2           | Ali    | 2                |
+-- | 3           | John   | 1                |
+-- | 4           | Doe    | 2                |
+-- +-------------+--------+------------------+
+
+-- Result table:
+-- +-------------+---------------+
+-- | project_id  | average_years |
+-- +-------------+---------------+
+-- | 1           | 2.00          |
+-- | 2           | 2.50          |
+-- +-------------+---------------+
+-- he average experience years for the first project is (3 + 2 + 1) / 3 = 2.00 and for the second project is (3 + 2) / 2 = 2.50
+
+-- My solution (attempt 1)
+SELECT t.project_id, ROUND(AVG(t.experience_years), 2) average_years
+FROM (
+        SELECT p.project_id, p.employee_id, e.experience_years
+        FROM Project p JOIN Employee e
+        ON p.employee_id = e.employee_id
+    ) t
+GROUP BY t.project_id
+
+-- Solution 2 (same as above, without subquery. This is better!)
+SELECT project_id, ROUND(AVG(experience_years), 2) average_years
+FROM Project p JOIN Employee e
+ON p.employee_id = e.employee_id
+GROUP BY project_id
+
+
+
 
 
 
